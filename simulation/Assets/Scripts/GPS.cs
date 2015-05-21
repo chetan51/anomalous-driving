e353bee4f02204a8de7c16d1bd985873a2260a33
Chetan Surpur <chetan51@gmail.com>
2015-05-20 17:00:19 -0700

    Fix timestamp

--- simulation/Assets/Scripts/GPS.cs
+++ simulation/Assets/Scripts/GPS.cs
@@ -5,10 +5,12 @@ using System;
 public class GPS : MonoBehaviour {
 
 	void Update() {
+		double timestamp = (System.DateTime.Now - new DateTime(1970, 1, 1).ToLocalTime()).TotalSeconds;
+
 		API.instance.SetOutput("x", gameObject.transform.position.x);
 		API.instance.SetOutput("y", gameObject.transform.position.y);
 		API.instance.SetOutput("z", gameObject.transform.position.z);
-		API.instance.SetOutput("timestamp", System.DateTime.Now);
+		API.instance.SetOutput("timestamp", timestamp);
 	}
 
 }
