f22dc7409d64856e3d3004e5cdad92e13b446824
Chetan Surpur <chetan51@gmail.com>
2015-05-21 19:09:49 -0700

    Added scale option to GPS

--- simulation/Assets/Scripts/GPS.cs
+++ simulation/Assets/Scripts/GPS.cs
@@ -4,12 +4,14 @@ using System;
 
 public class GPS : MonoBehaviour {
 
+	public float scale = 1.0f;
+
 	void Update() {
 		double timestamp = (System.DateTime.Now - new DateTime(1970, 1, 1).ToLocalTime()).TotalSeconds;
 
-		API.instance.SetOutput("x", gameObject.transform.position.x);
-		API.instance.SetOutput("y", gameObject.transform.position.y);
-		API.instance.SetOutput("z", gameObject.transform.position.z);
+		API.instance.SetOutput("x", gameObject.transform.position.x * scale);
+		API.instance.SetOutput("y", gameObject.transform.position.y * scale);
+		API.instance.SetOutput("z", gameObject.transform.position.z * scale);
 		API.instance.SetOutput("timestamp", timestamp);
 	}
 
